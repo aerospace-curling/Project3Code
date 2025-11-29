805b921bc7ab7cfe72a43af4b2231086f4d55801
aerospace-curling <emily.peelar@torontomu.ca>
2025-11-29 09:52:04 -0500

    Adding Contour Detection
    First it was attempted to use the original threshold, but this detected the desktop surface.  Therefore, an inverted threshold was used with a defined area.

--- Code-Project-3.py
+++ Code-Project-3.py
@@ -27,7 +27,7 @@ plt.show()
 
 
 
-#creating the binary threshold with blur
+#creating the binary threshold without blur
 _,threshold = cv2.threshold(gray_image, 120, 255, cv2.THRESH_BINARY)
 
 #showing the image with the binary threshold
@@ -111,4 +111,87 @@ plt.imshow(cv2.cvtColor(harris_copied_five, cv2.COLOR_BGR2RGB))
 plt.title("Harris Corner Detection with a Maximum value of 5%")
 plt.axis("off")
 plt.show()
+
+
+
+
+
+#after analyzing the methods above, it was decided that the blurred canny method was the optimal choice. This is because canny was able to determine continuous object edges, whereas harris only found the corner points 
+
+#first it was attempted to use the threshold, which resulted in the findings shown
+#now starting with contour detection, the blurred threshold is used again
+#findCountours is used with the blurred threshold defined previously, RETR_EXTERNAL to find the external contours, and CHAIN_APPROX_SIMPLE to store all contour points 
+contours, hierarchy = cv2.findContours(threshold_blurred,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+#then another copy of the image is made
+contour_image=image.copy()
+#drawContours is added to draw contours on the original image, coloured in green and a thickness of 2
+cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
+
+#now plotting the figure to see the results
+plt.figure()
+plt.imshow(cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB))
+plt.title("Contour Detection")
+plt.axis("off")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#an inverted threshold was added being that the original threshold was just picking up the edge of the desk
+_, threshold_inverted = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY_INV)
+#plotting with the inverted threshold
+plt.figure()
+plt.imshow(threshold_inverted, cmap='gray')
+plt.title("Inverted Binary Threshold")
+plt.axis('off')
+plt.show()
+
+#findCountours is used with the blurred threshold defined previously, RETR_EXTERNAL to find the external contours, and CHAIN_APPROX_SIMPLE to store all contour points 
+contours, hierarchy = cv2.findContours(threshold_inverted,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+#a minimum area was added to filter out small contours
+minimum_area=50000
+#an empty list was created to store the largest contours from the for loop
+largest_contours=[]
+#a for loop was created to loop through the contours to determine the largest one
+for contour in contours:
+    area = cv2.contourArea(contour)
+    if area > minimum_area:
+        largest_contours.append(contour)
+#checking if the list is empty.If it is, it is replaced with the original contours
+if len(largest_contours) == 0:
+    largest_contours = contours
+#finding the overall largest contour as obtained from the for loop, with the key selecting the contour with the greatest area
+overall_largest_contour = max(largest_contours, key=cv2.contourArea)
+
+#then another copy of the image is made
+contour_image=image.copy()
+#drawContours is added to draw the largest contour on the original image, coloured in green and a thickness of 2
+cv2.drawContours(contour_image, [overall_largest_contour], -1, (0, 255, 0), 2)
+
+#now plotting the figure to see the results
+plt.figure()
+plt.imshow(cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB))
+plt.title("Contour Detection with Inverted Threshold")
+plt.axis("off")
+plt.show()
+
+#the findings of the inverted method with area defined were far superior to those using just the original threshold
+
+        
+
+
+
+
+
 
